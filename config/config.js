6b108d6bfc8bc4465e98faaa7086ffbf070a7b2c
vituuk <ukvitu9999@gmail.com>
2026-06-29 15:31:23 +0700

    Switch to use with Neon

--- config/config.js
+++ config/config.js
@@ -6,13 +6,14 @@ module.exports =
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
+
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-
     }
   },
   "test": {
@@ -27,14 +28,14 @@ module.exports =
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
+
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-
     }
   }
 }
- 
